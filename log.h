4d13cc7e6cff7fd58652c99463079a36b4423d1b
potat <srenduch@ucdavis.edu>
2024-09-03 00:49:31 -0700

    Add timestamp to log macro

--- log.h
+++ log.h
@@ -4,15 +4,32 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdbool.h>
+#include <time.h>
+
+#define _GET_TIME() \
+    time_t time_since_epoch = time(NULL); \
+    struct tm* local = localtime(&time_since_epoch)
 
 #ifndef LOGGING_ENABLED
 #define LOG_OUT(fmt, ...) (void)0
 #define LOG_ERR(fmt, ...) (void)0
 #else
 #define LOG_OUT(fmt, ...) \
-    fprintf(stdout, fmt"\n", ##__VA_ARGS__)
+    do \
+    { \
+        _GET_TIME(); \
+        fprintf(stdout, "[%02d:%02d:%02d]\t"fmt"\n", \
+            local->tm_hour, local->tm_min, local->tm_sec \
+            ##__VA_ARGS__); \
+    } while (0)
 #define LOG_ERR(fmt, ...) \
-    fprintf(stderr, fmt"\n", ##__VA_ARGS__)
+    do \
+    { \
+        _GET_TIME(); \
+        fprintf(stderr, "[%02d:%02d:%02d]\t"fmt"\n", \
+            local->tm_hour, local->tm_min, local->tm_sec \
+            ##__VA_ARGS__); \
+    } while (0)
 #endif /* LOGGING_ENABLED */
 
 #endif /* __LOGGER_H__ */
