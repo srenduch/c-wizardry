2ce44a78f7397dd870e005d1bdc2e296a3e2cffc
potat <srenduch@ucdavis.edu>
2024-09-04 00:03:58 -0700

    Create logging thread and garbage collection

---
+++ log.c
@@ -0,0 +1,211 @@
+#include <stdlib.h>
+#include <pthread.h>
+
+#include "log.h"
+
+#define CHUNK_SIZE 16
+#define NUM_CHUNKS ((state.size / CHUNK_SIZE) + 1)
+
+enum active_state
+{
+    LOG_STATE_INACTIVE, /* Uninitialized */
+    LOG_STATE_ACTIVE, /* Initialized and ready */
+    LOG_STATE_DESTROY, /* Finish then cleanup */
+    LOG_STATE_FATAL, /* Finish and kill */
+    LOG_STATE_MAX,
+    LOG_STATE_INVALID = LOG_STATE_MAX
+};
+
+struct logging_state
+{
+    int write_pos; /* Queueing */
+    int read_pos; /* Dequeuing */
+    int size;
+    volatile enum active_state active;
+};
+
+/* Threading environment */
+static pthread_mutex_t state_sync_mutex;
+static pthread_t thread;
+
+/* Logging state */
+static struct log* queue;
+static struct logging_state state;
+
+/**
+ * Retrieve the oldest log in the queue
+ * @return Oldest log in queue if non-empty, else NULL
+ * @note This function is only called in the logging thread, so it does not
+ *  need to be atomically protected
+ */
+static struct log*
+log_dequeue
+(
+    void
+);
+
+/**
+ * Worker thread target that handles logging
+ */
+static void*
+log_message
+(
+    void*
+);
+
+/**
+ * Intermediate ISR that signals the logging thread to start tearing down
+ * the logging framework
+ * @param sig SIG_LOG_DESTROY must be passed to initiate destruction
+ */
+static void
+_destroy_logger
+(
+    int sig
+);
+
+void
+log_enqueue
+(
+    struct log log
+)
+{
+    if (state.active != LOG_STATE_ACTIVE)
+        return;
+
+    struct log* queue_copy;
+
+    queue_copy = queue;
+
+    pthread_mutex_lock(&state_sync_mutex);
+    if (state.size == NUM_CHUNKS * CHUNK_SIZE)
+    {
+        if ((queue = realloc(
+                queue_copy, sizeof(struct log) * (state.size + CHUNK_SIZE))))
+        {
+            free(queue_copy);
+        }
+        else
+        {
+            LOG_FATAL("Could not realloc queue: current size %d", state.size);
+        }
+    }
+    state.size++;
+    queue[state.write_pos] = log;
+    state.write_pos = (state.write_pos + 1) % (CHUNK_SIZE * NUM_CHUNKS);
+    pthread_mutex_unlock(&state_sync_mutex);
+}
+
+static struct log*
+log_dequeue
+(
+    void
+)
+{
+    /* todo: add realloc to shrink queue based on size to prevent memory blow up */
+    struct log* log;
+
+    pthread_mutex_lock(&state_sync_mutex);
+    state.size--;
+    log = &queue[state.read_pos];
+    state.read_pos = (state.read_pos + 1) % (CHUNK_SIZE * NUM_CHUNKS);
+    pthread_mutex_unlock(&state_sync_mutex);
+
+    return log;
+}
+
+static void*
+log_message
+(
+    void* arg
+)
+{
+    (void)arg; /* Stub */
+
+    while (true)
+    {
+        struct log* log;
+
+        if (state.active == LOG_STATE_ACTIVE)
+        {
+            if (!state.size)
+                continue;
+
+            log = log_dequeue();
+            if (log)
+                fprintf(log->stream, "%s", log->msg);
+        }
+        else if (state.active == LOG_STATE_DESTROY)
+        {
+            while (state.size && (log = log_dequeue()))
+                fprintf(log->stream, "%s", log->msg);
+
+            pthread_mutex_destroy(&state_sync_mutex);
+            free(queue);
+            break;
+        }
+        else if (state.active == LOG_STATE_FATAL)
+        {
+            while (state.size && (log = log_dequeue()))
+                fprintf(log->stream, "%s", log->msg);
+            pthread_exit(NULL);
+        }
+    }
+
+    return NULL;
+}
+
+static void
+_destroy_logger
+(
+    int sig
+)
+{
+    if (state.active == LOG_STATE_INACTIVE)
+        return;
+    
+    if (sig == SIG_LOG_DESTROY)
+    {
+        state.active = LOG_STATE_DESTROY;
+    }
+    else if (sig == SIG_LOG_FATAL)
+    {
+        state.active = LOG_STATE_FATAL;
+        pthread_exit(NULL);
+    }
+}
+
+void
+init_logger
+(
+    void
+)
+{
+    if (!(queue = malloc(sizeof(struct log) * CHUNK_SIZE)))
+        LOG_FATAL("Could not create logging queue");
+    
+    if (pthread_mutex_init(&state_sync_mutex, NULL))
+        LOG_FATAL("Error initializing logging mutex");
+    
+    state.active = LOG_STATE_ACTIVE;
+    if (pthread_create(&thread, NULL, log_message, NULL))
+        LOG_FATAL("Error creating logging thread");
+    if (pthread_detach(thread))
+        LOG_FATAL("Error detaching logging thread");
+    
+    signal(SIG_LOG_DESTROY, _destroy_logger);
+    signal(SIG_LOG_FATAL, _destroy_logger);
+
+    LOG_OUT("Logger initialization finished");
+}
+
+void
+destroy_logger
+(
+    void
+)
+{
+    LOG_OUT("Logger destruction started");
+    raise(SIG_LOG_DESTROY);
+    pthread_exit(NULL);
+}
--- log.h
+++ log.h
@@ -2,9 +2,15 @@
 #define __LOGGER_H__
 
 #include <stdio.h>
-#include <unistd.h>
-#include <stdbool.h>
 #include <time.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <stdbool.h>
+
+#define _MSG_MAX_LEN 1024
+
+#define SIG_LOG_DESTROY SIGUSR1
+#define SIG_LOG_FATAL SIGUSR2
 
 #define _GET_TIME() \
     time_t time_since_epoch = time(NULL); \
@@ -12,24 +18,75 @@
 
 #ifndef LOGGING_ENABLED
 #define LOG_OUT(fmt, ...) (void)0
-#define LOG_ERR(fmt, ...) (void)0
+
+#define LOG_NONFATAL(fmt, ...) (void)0
+
+#define LOG_FATAL(fmt, ...) (void)0
 #else
+
 #define LOG_OUT(fmt, ...) \
     do \
     { \
+        struct log log; \
+    \
+        log.stream = stdout; \
         _GET_TIME(); \
-        fprintf(stdout, "[%02d:%02d:%02d]\t"fmt"\n", \
-            local->tm_hour, local->tm_min, local->tm_sec \
+        snprintf(log.msg, _MSG_MAX_LEN, "[%02d:%02d:%02d]\t"fmt"\n", \
+            local->tm_hour, local->tm_min, local->tm_sec, \
             ##__VA_ARGS__); \
+        log_enqueue(log); \
     } while (0)
+
 #define LOG_ERR(fmt, ...) \
     do \
     { \
+        struct log log; \
+    \
+        log.stream = stderr; \
         _GET_TIME(); \
-        fprintf(stderr, "[%02d:%02d:%02d]\t"fmt"\n", \
-            local->tm_hour, local->tm_min, local->tm_sec \
+        snprintf(log.msg, _MSG_MAX_LEN, "[%02d:%02d:%02d]\t"fmt"\n", \
+            local->tm_hour, local->tm_min, local->tm_sec, \
             ##__VA_ARGS__); \
+        log_enqueue(log); \
+    } while (0)
+
+#define LOG_FATAL(fmt, ...) \
+    do \
+    { \
+        LOG_ERR("FATAL ERROR: "fmt, ##__VA_ARGS__); \
+        raise(SIG_LOG_FATAL); \
     } while (0)
 #endif /* LOGGING_ENABLED */
+
+struct log
+{
+    FILE* stream;
+    char msg[_MSG_MAX_LEN];
+};
+
+/**
+ * Enqueue a log into the logging queue
+ * @param log The log to enqueue
+ * @note Throws exception on error
+ * @note This function can be called in many contexts, so writing to the state
+ *  must be protected
+ */
+void
+log_enqueue
+(
+    struct log
+);
+
+void
+init_logger
+(
+    void
+);
+
+void
+destroy_logger
+(
+    void
+);
 
 #endif /* __LOGGER_H__ */
